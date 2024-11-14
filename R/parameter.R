ce3f216c114f30d3ed565c1a19e00fda4c04b861
Yezhi Pan <74977531+xavienzo@users.noreply.github.com>
2024-11-14 14:31:51 -0500

    ICONS_0.1.2

--- R/parameter.R
+++ R/parameter.R
@@ -8,47 +8,93 @@
 #' @param data A data matrix used in the SigmaU function.
 #' @param prctile_vec A vector of percentiles for the pairwise potentials to consider.
 #' @param lam_vec A vector of lambda values to test.
+#' @param method A string indicating the method to use for SigmaU calculation (default is "Sample").
+#' @param ncores The number of cores to use for parallel processing (default is NULL, which uses all available cores - 1).
+#' @param use_parallel A logical value indicating whether to use parallel processing. Defaults to TRUE.
 #'
 #' @return A list containing the optimal lambda value (`lambda_out`) and the corresponding cut-off value (`cut_out`).
 #' @export
-#'
-#' @examples
-#' data <- matrix(runif(200), nrow = 20)
-#' Wp <- cor(data)
-#' prctile_vec <- c(0.1, 0.5, 0.8)
-#' lam_vec <- c(0.1, 0.5, 1.0)
-#' result <- param_tuning_sigmau(Wp, data, prctile_vec, lam_vec)
-#' print(result)
-#'
-param_tuning_sigmau <- function(Wp, data, prctile_vec, lam_vec) {
+#' @importFrom parallel makeCluster detectCores clusterExport parLapply stopCluster mclapply
+#' @importFrom stats quantile
+
+param_tuning_sigmau <- function(Wp, data, prctile_vec, lam_vec, method = "Sample", ncores = NULL, use_parallel = TRUE) {
   # Convert pairwise potential matrix to a vector of non-redundant entries
   nlogp <- get_vectorform(as.matrix(Wp))
 
   # Calculate cut-off values based on the given percentiles
-  gr <- quantile(nlogp, prctile_vec / 100) # percentiles should be in [0, 1]
+  gr <- quantile(nlogp, prctile_vec / 100)  # percentiles should be in [0, 1]
 
   nodeLen <- nrow(Wp)
   edgeLen <- length(nlogp)
   sigmau_vec <- matrix(0, nrow = length(lam_vec), ncol = length(gr))
 
-  # Selection of lambda0 begins here
-  for (i in seq_along(lam_vec)) {
-    lambda <- lam_vec[i]
+  # Set up parallel processing (if ncores is provided)
+  if (is.null(ncores)) {
+    ncores <- detectCores() - 1  # Use one less than the total number of cores
+  }
 
-    for (j in seq_along(gr)) {
-      r <- gr[j]
+  # Define the function for parameter tuning for a given combination of lambda and cut-off
+  tune_param_set <- function(lambda, r) {
+    # Perform greedy clustering with the current cut-off and lambda
+    result <- dense(Wp, r, lambda)
+    Clist_temp <- result$Clist
+    CID_temp <- result$CID
 
-      # Perform greedy clustering with the current cut-off and lambda
-      result <- dense(Wp, r, lambda)
-      Clist_temp <- result$Clist
-      CID_temp <- result$CID
+    # Calculate SigmaU for the current clustering
+    sigmau_result <- scfa(data, CID_temp, Clist_temp, method = method)
+    sigmau_norm <- sigmau_result$sigma_u_norm
+    sigmau_norm_d <- sigmau_result$sigma_u_diag_norm
+    # Store the SigmaU value with a penalty for the number of clusters
+    return(sigmau_norm + sigmau_norm_d)
+  }
 
-      # Calculate SigmaU for the current clustering
-      sigmau_result <- get_sigmau(data, CID_temp, Clist_temp)
-      sigmau_norm <- sigmau_result$sigma_u_norm
-      sigmau_norm_d <- sigmau_result$sigma_u_diag_norm
-      # Store the SigmaU value with a penalty for the number of clusters
-      sigmau_vec[i, j] <- sigmau_norm + sigmau_norm_d#+ log(length(CID_temp))
+  # Check if parallel processing is enabled
+  if (use_parallel) {
+    # Check the operating system and set up parallel computing
+    if (.Platform$OS.type == "windows") {
+      # Windows system: use makeCluster
+      cl <- makeCluster(ncores)
+
+      # Export necessary variables to the cluster (if needed)
+      clusterExport(cl, list("Wp", "data", "gr", "lam_vec", "dense", "scfa", "method", "tune_param_set"))
+
+      # Parallel processing using parLapply
+      results <- parLapply(cl, seq_along(lam_vec), function(i) {
+        lambda <- lam_vec[i]
+        sapply(gr, function(r) tune_param_set(lambda, r))
+      })
+
+      # Stop the cluster after parallel computation
+      stopCluster(cl)
+    } else {
+      # macOS/Linux system: use mclapply (supports multicore)
+      results <- mclapply(seq_along(lam_vec), function(i) {
+        lambda <- lam_vec[i]
+        sapply(gr, function(r) tune_param_set(lambda, r))
+      }, mc.cores = ncores)
+    }
+
+    # Convert the list of results into a matrix
+    sigmau_vec <- do.call(rbind, results)
+  } else {
+    # Run the tuning process without parallel processing (sequential mode)
+    for (i in seq_along(lam_vec)) {
+      lambda <- lam_vec[i]
+      for (j in seq_along(gr)) {
+        r <- gr[j]
+
+        # Perform greedy clustering with the current cut-off and lambda
+        result <- dense(Wp, r, lambda)
+        Clist_temp <- result$Clist
+        CID_temp <- result$CID
+
+        # Calculate SigmaU for the current clustering
+        sigmau_result <- scfa(data, CID_temp, Clist_temp, method = method)
+        sigmau_norm <- sigmau_result$sigma_u_norm
+        sigmau_norm_d <- sigmau_result$sigma_u_diag_norm
+        # Store the SigmaU value with a penalty for the number of clusters
+        sigmau_vec[i, j] <- sigmau_norm + sigmau_norm_d
+      }
     }
   }
 
--- R/scfa.R
+++ R/scfa.R
@@ -5,40 +5,88 @@
 #' @param data A data frame or matrix where rows represent observations and columns represent variables.
 #' @param CID A numeric vector indicating the number of variables associated with each factor.
 #' @param Clist A numeric vector indicating the column indices of the variables to be used in the analysis.
+#' @param method Character. Method of residual term estimation: "Sample" or "MLE".
 #'
 #' @return A list containing:
-#' \item{loading}{A matrix of factor loadings.}
-#' \item{factorscore}{A matrix of estimated factor scores.}
-#' \item{n.network}{Integer. Number of factors.}
-#' \item{n.var}{Integer. Number of variables.}
-#' \item{n.obs}{Integer. Number of observations.}
+#' \describe{
+#'   \item{loading}{A matrix of factor loadings.}
+#'   \item{factorscore}{A matrix of estimated factor scores.}
+#'   \item{sigma_u}{Matrix of off-diagonal residuals (Sample or MLE).}
+#'   \item{sigma_u_norm}{Frobenius norm of sigma_u.}
+#'   \item{sigma_u_diag}{Diagonalized residuals (Sample or MLE).}
+#'   \item{sigma_u_diag_norm}{Frobenius norm of sigma_u_diag.}
+#'   \item{n.network}{Integer. Number of factors.}
+#'   \item{n.var}{Integer. Number of variables.}
+#'   \item{n.obs}{Integer. Number of observations.}
+#'   \item{method}{The chosen estimation method.}
+#' }
 #'
 #' @export
+#' @importFrom stats cov
 
-scfa <- function(data, CID, Clist) {
-  CID_len <- length(CID)
-  k <- CID_len - 1
+scfa <- function(data, CID, Clist, method = "Sample") {
+
+  method <- match.arg(method, choices = c("Sample", "MLE"))
+
+  # parameters
+  k0 <- length(CID)
+  k <- k0 - 1
   n <- nrow(data)
+  CID_temp <- CID[-k0]
+  p <- sum(CID_temp)
+  p0 <- ncol(data)
 
-  CID_temp <- CID[-CID_len]
-  clist_in <- get_index(1:k, CID, Clist)[[1]]
-  p <- length(clist_in)
-
+  # Set up factor loading matrix
   L <- matrix(0, p, k)
   indices <- rep(1:k, CID_temp)
   L[cbind(1:p, indices)] <- 1
+  LT <- t(L)
 
-  X <- data[, clist_in]
-  XT <- t(X)
-  XT_mean <- apply(XT, 1, mean)
-  XT_cent <- XT - matrix(rep(XT_mean, n), p, n, byrow = FALSE)
+  # Data centering for selected columns
+  X0 <- data[, Clist, drop = FALSE]
+  XT0_cent <- t(sweep(X0, 2, colMeans(X0)))
+  XT_cent <- XT0_cent[1:p, , drop = FALSE]
 
-  F_HAT <- solve(t(L) %*% L) %*% (t(L) %*% XT_cent)
+  F_HAT <- solve(LT %*% L) %*% (LT %*% XT_cent)
+
+  SIGMA_F <- cov(t(F_HAT))
+  L_SIGMAF_LT <- L %*% SIGMA_F %*% LT
+
+  if (method == "Sample") {
+    U <- t(XT_cent) - t(F_HAT) %*% LT
+    SIGMA_U <- diag(diag(cov(U)))
+  } else {
+    SIGMA_U <- diag(diag(cov(t(XT_cent)) - L_SIGMAF_LT))
+  }
+
+  F_HAT_FINAL <- solve(LT %*% solve(SIGMA_U) %*% L) %*% LT %*% solve(SIGMA_U) %*% XT_cent
+
+  ################ GET SIGMAU #################
+  SIGMA_X0 <- cov(t(XT0_cent))
+  SIGMA_F <- cov(t(F_HAT_FINAL))
+  L_SIGMAF_LT_full <- matrix(0, p0, p0)
+  L_SIGMAF_LT_full[1:p, 1:p] <- L %*% SIGMA_F %*% t(L)
+
+  SIGMA_U_OFF <- SIGMA_X0 - L_SIGMAF_LT_full
+  diag(SIGMA_U_OFF) <- 0
+  SIGMA_UF_OFF <- norm(SIGMA_U_OFF, type = "F")
+
+  ################ GET SIGMAU 0 (DIAGNAL) #################
+  SIGMA_F0 <- if (k > 1) diag(diag(SIGMA_F)) else SIGMA_F
+  L_SIGMAF_LT_full[1:p, 1:p] <- L %*% SIGMA_F0 %*% t(L)
+
+  SIGMA_U_DIAG <- SIGMA_X0 - L_SIGMAF_LT_full
+  diag(SIGMA_U_DIAG) <- 0
+  SIGMA_UF_DIAG <- norm(SIGMA_U_DIAG, type = "F")
 
   return(list(loading = L,
               factorscore = F_HAT,
-              #x = X,
+              sigma_u = SIGMA_U_OFF,
+              sigma_u_norm = SIGMA_UF_OFF,
+              sigma_u_diag = SIGMA_U_DIAG,
+              sigma_u_diag_norm = SIGMA_UF_DIAG,
               n.network = k,
               n.var = p,
-              n.obs = n))
+              n.obs = n,
+              method = method))
 }
